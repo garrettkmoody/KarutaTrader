31c8a55806c0e085f628353621e198535b781365
Garrett Moody <extrillieos@gmail.com>
2021-04-01 01:29:40 -0700

    More trade completion

--- bot.py
+++ bot.py
@@ -2,16 +2,15 @@
 import os
 import random
 import discord
+from tradePackage import TradePackage
 from discord.ext import commands
-TOKEN = "ODI2NzE1MDUxMDE1Nzk4Nzk0.YGQgYw.h5cxPUt_djPTsHt2Lnev0TIwuUw"
+TOKEN = "ODI2NzE1MDUxMDE1Nzk4Nzk0.YGQgYw.GnItg9EEHZnMI0d7FKp9FbVHIy8"
 GUILDNUM = "706297495122214984"
 client = discord.Client()
 bot = commands.Bot(command_prefix='x')
 bot.remove_command('help')
+tradePack = None
 lastMessage = None
-lastEmbed = None
-replace = None
-
 
 async def on_ready():
     for guild in client.guilds:
@@ -26,36 +25,32 @@ async def on_ready():
     print(f'Guild Members:\n - {members}')
 
 @bot.command(name="trade")
-async def trade(ctx, arg1, arg2, arg3, arg4):
+async def trade(ctx, arg1, arg2, arg3):
+    global tradePack
     global lastMessage
-    global lastEmbed
-    global replace
-    if arg4 == 'b':
-        embedVar = discord.Embed(title="Trade Outline", color=0xFF9900)
-        embedVar.add_field(name=ctx.author, value="***PRICE*** : `${}`".format(arg3), inline=False)
-        replace = embedVar.add_field(name="No User Selected", value="***CARD*** : `{}`".format(arg1), inline=False)
+    if arg3 == 'b':
         await ctx.send("Beginning a trade: \n")
-        theMessage = await ctx.send(embed=embedVar)
-        lastEmbed = embedVar
+        tradePack = TradePackage(arg2,arg1,ctx.author)
+        theMessage = await ctx.send(embed=tradePack.get_embed())
         lastMessage = theMessage
         await theMessage.add_reaction("💳")
-    elif arg4 == 'a':
+    elif arg3 == 'a':
         print("holder")
     else:
         await ctx.send("Please enter a `b` for BID or `a` for ASK")
 
 @bot.event
 async def on_reaction_add(reaction, user):
-  global lastEmbed
-  if user == bot.user:
-      return
-  if reaction.emoji == '💳':
-    lastEmbed.fields[1] = None
-    lastEmbed.add_field(name=user, value="***CARD*** : `{}`".format("buut"), inline=False)
-    await lastMessage.channel.send(embed=lastEmbed)
+    global tradePack
+    global lastMessage
+    if user == bot.user:
+        return
+    if reaction.emoji == '💳':
+        tradePack.set_bidderName(user)
+        await lastMessage.edit(embed=tradePack.get_embed(2))
 
 @bot.command(name="help")
 async def help(ctx):
-    await ctx.send("Commands available:\n" + "`xtrade <CardCode> <Other user> <Price> <b/a>`")
+    await ctx.send("Commands available:\n" + "`xtrade <CardCode> <Price> <b/a>`")
 
 bot.run(TOKEN)
---
+++ tradePackage.py
@@ -0,0 +1,46 @@
+import discord
+
+class TradePackage:
+    def __init__(self, price, card, askerName = "No user selected", bidderName = "No user selected"):
+        self.bidderName = bidderName
+        self.askerName = askerName
+        self.bidderUser = None
+        self.askerUser = None
+        self.price = price
+        self.card = card
+        self.venmo = None
+
+    def get_bidderName(self):
+        return self.bidderName
+    def get_askerName(self):
+        return self.askerName
+    def get_bidderUser(self):
+        return self.bidderUser
+    def get_askerUser(self):
+        return self.askerUser
+    def set_bidderName(self, newName):
+        self.bidderName = newName
+    def set_askerName(self, newName):
+        self.askerName = newName
+    def set_bidderUser(self, newUser):
+        self.bidderUser = newUser
+    def set_askerUser(self, newUser):
+        self.askerUser = newUser
+
+    def get_embed(self, stage=1):
+        if stage == 1:
+            embedVar = discord.Embed(title="Trade Outline", color=0xFF9900)
+            embedVar.add_field(name=self.askerName, value="***PRICE*** : `${}`".format(self.price), inline=False)
+            embedVar.add_field(name=self.bidderName, value="***CARD*** : `{}`".format(self.card), inline=False)
+        elif stage == 2:
+            embedVar = discord.Embed(title="Trade Completion", color=0xFF9900)
+            embedVar.add_field(name =self.askerName, value= "❌", inline = True)
+            embedVar.add_field(name = '`Task`', value = "Send `{}` card to this bot".format(self.card), inline = True)
+            embedVar.add_field(name = 'Status', value = "❌", inline = True)
+
+            embedVar.add_field(name = self.bidderName, value = "❌", inline = True)
+            embedVar.add_field(name = '`Task`', value = "Send $`{}` to this Venmo".format(self.price), inline = True)
+            embedVar.add_field(name = 'Status', value = "❌", inline = True)
+
+            embedVar.set_footer(text=self.askerName)
+        return embedVar
